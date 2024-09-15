266915bd27fc56354cf88e3714fe93fa4e9f7dd0
Priyanka Yadav <priyankayadav8120@gmail.com>
2024-09-16 01:56:13 +0530

    Add messageController for rest api call, SimpMessageController for stomp call

--- src/main/java/com/project/SmartChat/controller/MessageController.java
+++ src/main/java/com/project/SmartChat/controller/MessageController.java
@@ -1,96 +1,56 @@
 package com.project.SmartChat.controller;
 
-import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.SmartChat.model.Group;
 import com.project.SmartChat.model.Message;
-import com.project.SmartChat.model.User;
 import com.project.SmartChat.model.dto.MessageDTO;
 import com.project.SmartChat.service.GroupService;
 import com.project.SmartChat.service.MessageService;
-import com.project.SmartChat.service.ParticipantService;
-import com.project.SmartChat.service.UserService;
 
 
-@Controller
+@RestController
+@RequestMapping("/api/groups/{groupId}/messages")
 public class MessageController {
-    private final SimpMessagingTemplate messagingTemplate;
-
-    @Autowired
-    private MessageService messageService;
-
-    @Autowired
-    private ParticipantService participantService;
-
+    
     @Autowired
     private GroupService groupService;
 
     @Autowired
-    private UserService userService;
+    private MessageService messageService;
 
-    public MessageController(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
+
+    // Get all messages by groupID
+    @GetMapping("")
+    public ResponseEntity<List<MessageDTO>> getGroupById(@PathVariable("groupId") Long groupId) {
+        Group group = groupService.getGroupById(groupId);
+        if (group == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Make a list of all Messages related to this group
+        List<Message> messages = messageService.getByGroupId(groupId);
+
+        List<MessageDTO> messageDTOs = messages.stream().map(message -> {
+            MessageDTO messageDTO = new MessageDTO();
+            messageDTO.setId(message.getId());
+            messageDTO.setGroupId(message.getGroup().getId());
+            messageDTO.setSenderId(message.getSender().getId());
+            messageDTO.setContent(message.getContent());
+            messageDTO.setIsSeen(message.getIsSeen());
+            messageDTO.setSentAt(message.getSentAt());
+            return messageDTO;
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.ok(messageDTOs);
     }
 
-
-    // send message support
-    // Limitation: As of now, one to one message is only supported
-    @MessageMapping("/chat.sendMessage")
-    public void sendMessage(@RequestBody MessageDTO messageDto, SimpMessageHeaderAccessor headerAccessor, Authentication authentication) {
-        System.out.println("----------------------------------------------------------------------");
-        System.out.println("Inside ChatController.sendMessage");
-        System.out.println("----------------------------------------------------------------------");
-        // TODO: check if authentication is set
-       
-        Long senderId = messageDto.getSenderId();
-        Long sendToGroupId = messageDto.getGroupId();
-        String content = messageDto.getContent();
-
-        // get sender
-        User sender = userService.findByUserId(senderId).get();
-
-        if (sender == null) {
-            System.out.println("Provided sender does not exist in database");
-            return;
-        }
-
-        // Check if this groupUser is member of the group 'sendToGroup'
-        if (!participantService.isUserIdParticipantOfGroupId(senderId, sendToGroupId)) {
-            System.out.println("sender is not a member of the group");
-            return;
-        }
-        // sender 'sender' is part of the group 'sendToGroup'
-
-        Group sendToGroup = groupService.getGroupById(sendToGroupId);
-        if (sendToGroup == null) {
-            System.out.println("Group does not exist...............");
-            return;
-        }
-
-        System.out.println("Content " + messageDto.getContent());
-        List<User> groupUsers =  participantService.getUsersByGroupId(sendToGroupId);
-
-        Message message = new Message();
-        message.setSender(sender);
-        message.setGroup(sendToGroup); 
-        message.setContent(content);
-        message.setSentAt(new Date());
-        
-        message = messageService.saveMessage(message);
-        messageDto.setSentAt(message.getSentAt());
-        messageDto.setId(message.getId());
-        
-        String destinatioString ="/queue/messages/groups";
-        System.out.println("destinatioString: " + destinatioString);
-        messagingTemplate.convertAndSendToUser("groupId-" + sendToGroupId, destinatioString, messageDto);
-    }
 }
